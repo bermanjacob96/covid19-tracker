58156982499624e632044975748c40bd096ee5bc
Jacob Berman <jacob.berman@oildex.com>
2020-06-07 23:45:40 -0600

    Added Bootstrap, homeComponent, countriesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CountriesComponent } from './components/countries/countries.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CountriesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
